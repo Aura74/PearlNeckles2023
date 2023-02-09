d1c5105a4cbe53d597c24722eaa6750ad5495f35
Lars Asplund <lars.asplund@hotmail.com>
2023-02-09 08:55:45 +0100

    Add project files.

---
+++ PearlNeckles2023/IPearl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PearlNeckles2023
+{
+    public enum PearlColor { Black, White, Pink }
+    public enum PearlShape { Round, DropShaped }
+    public enum PearlType { FreshWater, SaltWater }
+    public interface IPearl
+    {
+        public int Size { get; set; }
+        public PearlColor Color { get; set; }
+        public PearlShape Shape { get; set; }
+        public PearlType Type { get; set; }
+        public decimal Price { get; }
+    }
+}
---
+++ PearlNeckles2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PearlNeckles2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
